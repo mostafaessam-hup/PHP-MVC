baaaed861155d283eaf021c67342aba5bcf943b9
Mostafa Essam <mostafaessam540@gmail.com>
2023-01-29 01:15:55 +0200

    second commit

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -4,13 +4,16 @@ namespace Http;
 
 class Request
 {
+    /**
+     * try something
+     */
     public function method()
     {
         return strtolower($_SERVER["REQUEST_METHOD"]);
     }
     public function path()
     {
-       $path= $_SERVER["request_uri"]??"/";
-       return str_contains($path,"?") ? explode("?",$path[0]):$path;
+        $path = $_SERVER["request_uri"] ?? "/";
+        return str_contains($path, "?") ? explode("?", $path[0]) : $path;
     }
 }
